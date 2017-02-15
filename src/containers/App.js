9615023c302ed9522a41f4ee09c19a0c01477688
Михаил Ю. Плюснин <plyusnin@iac.local>
2017-02-15 15:55:45 +0500

    настроил react-router

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,20 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router';
 
 export default class App extends Component {
+    constructor(props) {
+        super(props);
+    }
+
     render() {
-        return <div className='container'>Привет из App!</div>
+        return <div className='container'>
+            <h1>App</h1>
+            <ul>
+                <li><Link to='/22'>Home</Link></li>
+                <li><Link to='/admin'>Admin</Link></li>
+                <li><Link to='/genre'>Genre</Link></li>
+            </ul>
+            {this.props.children}
+        </div>
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import 'babel-polyfill'
 import React from 'react'
 import { render } from 'react-dom'
-import App from './containers/App'
+import { Router, Route, browserHistory } from 'react-router';
+import Routes from './Utils/routes'
+import NotFound from './components/NotFound';
 
 render(
-    <App />,
+    <Router history={browserHistory}>
+        {Routes}
+        <Route path='*' component={NotFound}></Route>       
+    </Router>,
     document.getElementById('root')
 )
